46e2bdc5dec72e30ae2ec1253ee05e920eab7c9a
Mirka <mirkanicolle@gmail.com>
2022-09-01 00:47:19 -0600

    lsr init

---
+++ dvr/lsr.py
@@ -0,0 +1,30 @@
+'''
+Laboratorio 3 - Algoritmos de enrutamiento
+
+Mirka Monzon 18139
+Oscar De Leon 19298
+Andres Quinto 19288
+'''
+import slixmpp
+
+class LSR(slixmpp.ClientXMPP):
+    def __init__(jid, password,topolo,name):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+        self.add_event_handler("session_start", self.start)
+        self.add_event_handler("message", self.message)
+        
+        self.topolo = topolo
+        self.name = name
+        self.network = []
+        self.echo_sent = None
+        self.LSP = {
+            'type': lsp,
+            'from': self.boundjid.bare,
+            'sequence': 1,
+            'neighbours':{}
+        }
+        self.id = get_ID(self.name, jid)
+        self.neighbours_IDS = get_neighbors(self.topoLO, self.id)
+        self.neighbours = []
+        self.neighbours_JID()
+
---
+++ dvr/main.py
@@ -0,0 +1,23 @@
+'''
+Laboratorio 3 - Algoritmos de enrutamiento
+
+Mirka Monzon 18139
+Oscar De Leon 19298
+Andres Quinto 19288
+'''
+
+from getpass import getpass
+from dvr.lsr import LRS, LSR
+from lsr import *
+
+#Main
+if __name__ == '__main__':
+    print("\n--- Chatea :) ---\n")
+    jid = input('Ingresa tu jid: ')
+    password = getpass('Ingresa tu contraseña: ')
+    name = input('Ingresa tu nombre(s): ')
+    topolo = input('Ingresa tu topologia: ')
+
+    xmpp = LSR(jid, password,topolo,name)
+    xmpp.connect()
+    xmpp.process(forever=False)
