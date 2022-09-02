99e0c5dd3b471686ce10686fa4604b154aa8e2de
Mirka <mirkanicolle@gmail.com>
2022-09-01 18:36:51 -0600

    fix lsr

--- lsr/lsr.py
+++ lsr/lsr.py
@@ -1,27 +1,21 @@
-'''
-Laboratorio 3 - Algoritmos de enrutamiento
-
-Mirka Monzon 18139
-Oscar De Leon 19298
-Andres Quinto 19288
-'''
-import slixmpp
+import asyncio
 from asyncio.tasks import sleep
+#from curses import echo
+import slixmpp
 from getpass import getpass
-from aioconsole import ainput 
+from aioconsole import ainput, aprint 
 import time
-from execute import *
+from utils import *
+from utils import echo_send, echo_response, lsp, json_to_object, message_type, hello, object_to_json
 
-#LSR clinte
-class LSR(slixmpp.ClientXMPP):
-    def __init__(self, jid, password,topolo,name):
+class LSRClient(slixmpp.ClientXMPP):
+    def __init__(self, jid, password, topo_file,names_file):
         slixmpp.ClientXMPP.__init__(self, jid, password)
-        self.add_event_handler("session_start", self.inicio)
+        self.add_event_handler("session_start", self.start)
         self.add_event_handler("message", self.message)
         
-        #definicon de variables para el chata
-        self.topolo = topolo
-        self.name = name
+        self.topo_file = topo_file
+        self.names_file = names_file
         self.network = []
         self.echo_sent = None
         self.LSP = {
@@ -30,66 +24,61 @@ class LSR(slixmpp.ClientXMPP):
             'sequence': 1,
             'neighbours':{}
         }
-        self.id = get_ID(self.name, jid)
-        self.IDS_vecinos = get_neighbors(self.topolo, self.id)
-        self.vecino = []
-        self.JID_vecinos()
+        self.id = get_ID(self.names_file, jid)
+        self.neighbours_IDS = get_neighbors(self.topo_file, self.id)
+        self.neighbours = []
+        self.neighbours_JID()
 
-    #funcion de inicio, para correr el chat 
-    async def inicio(self):
+    async def start(self, event):
         self.send_presence()
         await self.get_roster()
-        print("Presiona enter para empezar:")
-        inicio = await ainput()
-        for vecino in self.vecino:
-            await self.send_hello_msg(vecino)
-        for vecino in self.vecino:
-            await self.send_echo_message(vecino, echo_send)
+        print("Press enter to start:")
+        start = await ainput()
+        for neighbour in self.neighbours:
+            await self.send_hello_msg(neighbour)
+        for neighbour in self.neighbours:
+            await self.send_echo_message(neighbour, echo_send)
 
-        #inicio de network
         self.network.append(self.LSP) 
+
         self.loop.create_task(self.send_LSP())
+
         await sleep(2)
 
-        #Definicion de mensaje a enviar
-        print("Ingrese el jid del usuario al que desea enviar un mensaje: ")
+        print("Type the jid of the user you want to message (or wait until someone messages you!)")
         send = await ainput()
         if send != None:
-            message = await ainput("Ingresa el mensaje: ")
+            message = await ainput('Type your message: ')
 
-        #Se espera a la red
-        print("Esperando a la red")
+        #Waiting some time so that the network converges
+        print("Waiting for network to converge")
         await sleep(17)
-        print("Conexion correcta! enviando mensaje")
+        print("Network converged, sending message")
 
-        #Envio de mensaje 
         self.send_chat_message(self.boundjid.bare,send,steps=1,visited_nodes=[self.boundjid.bare],message=message)
-
-        #Terminar el proceso
-        print("Presiona enter para terminar")
+        
+        print("press enter to exit")
         exit = await ainput()
         self.disconnect()
 
-    #Funcion de obtencion de vecinos
-    def JID_vecinos(self):
-        for id in self.IDS_vecinos:
-            JID_vecinos = get_JID(self.name, id)
-            self.vecino.append(JID_vecinos)
+    def neighbours_JID(self):
+        for id in self.neighbours_IDS:
+            neighbour_JID = get_JID(self.names_file, id)
+            self.neighbours.append(neighbour_JID)
 
-    #Funcion de mensaje
     async def message(self, msg):
         body = json_to_object(msg['body'])
-        if body['type'] == hello: #caso de hello
-            print("Hola de : ", msg['from'])
+        if body['type'] == hello:
+            print("Hello from: ", msg['from'])
 
-        #caso contrario de hello
+
         elif body['type'] == echo_send:
-            print("Haciendo eco de: ", msg['from'])
+            print("Echoing back to: ", msg['from'])
             await self.send_echo_message(body['from'],echo_response)
 
         elif body['type'] == echo_response:
             distance = time.time()-self.echo_sent
-            print("Distancia de ", msg['from'], ' es ', distance)
+            print("Distance to ", msg['from'], ' is ', distance)
             self.LSP['neighbours'][body['from']] = distance
 
         elif body['type'] == lsp:
@@ -98,14 +87,14 @@ class LSR(slixmpp.ClientXMPP):
 
         elif body['type'] == message_type:
             if body['to'] != self.boundjid.bare:
-                print('Recibí un mensaje que no es para mí, lo envié ')
+                print('Got a message that is not for me, sending it ')
                 self.send_chat_message(source = body['from'],to = body['to'], steps=body['steps'] +1, distance=body['distance'],visited_nodes= body['visited_nodes'].append(self.boundjid.bare),message=body['mesage'])
             elif body['to'] == self.boundjid.bare:
-                print('Recibi un mensaje!')
+                print('Got a message!')
                 print(body['from'], " : ", body['mesage'])
                 print(body)
 
-    #Funcion para envio de mensaje    
+        
     async def send_hello_msg(self,to, steps = 1):
         you = self.boundjid.bare
         to = to 
@@ -118,7 +107,6 @@ class LSR(slixmpp.ClientXMPP):
         to_send = object_to_json(json)
         self.send_message(mto = to, mbody=to_send, mtype='chat')
     
-    #Funcion para eco de mensaje
     async def send_echo_message(self, to, type ,steps = 1):
         you = self.boundjid.bare
         to = to 
@@ -132,17 +120,15 @@ class LSR(slixmpp.ClientXMPP):
         self.send_message(mto = to, mbody=to_send, mtype='chat')
         self.echo_sent = time.time()
 
-    #Funcion envio de LSP
     async def send_LSP(self):
         while True:
-            for vecino in self.vecino:
+            for neighbour in self.neighbours:
                 lsp_to_send = object_to_json(self.LSP)
-                self.send_message(mto =vecino,mbody=lsp_to_send,mtype='chat')
+                self.send_message(mto =neighbour,mbody=lsp_to_send,mtype='chat')
             await sleep(2)
             self.LSP['sequence'] += 1
     
-    #Funcion envio de mensaje de chat
-    def send_chat_message(self,source,to,steps=0, distance = 0, visited_nodes = [],message="Hola chat"):
+    def send_chat_message(self,source,to,steps=0, distance = 0, visited_nodes = [],message="Hola mundo"):
         body ={
             'type':message_type,
             'from': source,
@@ -157,7 +143,6 @@ class LSR(slixmpp.ClientXMPP):
         to_send = object_to_json(body)
         self.send_message(mto=path[1]['from'],mbody = to_send,mtype='chat')
 
-    #Funcion para update red
     async def update_network(self, lsp):
         for i in range(0,len(self.network)):
             node = self.network[i]
@@ -171,22 +156,21 @@ class LSR(slixmpp.ClientXMPP):
         self.network.append(lsp)
         return 1
     
-    #Funcion para obtener el path
     def calculate_path(self, source, dest):
         distance = 0
         visited = []
         current_node = self.find_node_in_network(source)
         while current_node['from'] != dest:
             node_distances = [] 
-            vecino = current_node['neighbours']
-            for neighbour in vecino.keys():
+            neighbours = current_node['neighbours']
+            for neighbour in neighbours.keys():
                 if neighbour == dest:
                     visited.append(current_node)
                     current_node = self.find_node_in_network(neighbour)
                     visited.append(current_node)
                     return visited
                 elif neighbour not in visited:
-                    distance_to_neighbour = vecino[neighbour]
+                    distance_to_neighbour = neighbours[neighbour]
                     node_distances.append(distance_to_neighbour)
             min_distance = min(node_distances)
             node_index = node_distances.index(min_distance)
@@ -198,7 +182,6 @@ class LSR(slixmpp.ClientXMPP):
             distance += min_distance
         return visited
 
-    #Funcion para encontrar nodo en la red
     def find_node_in_network(self, id):
         for i in range(len(self.network)):
             node = self.network[i]
@@ -206,8 +189,7 @@ class LSR(slixmpp.ClientXMPP):
                 return node
         return False
 
-    #Funcion para LSP flood
     async def flood_LSP(self, lsp, new):
-        for vecino in self.vecino:
-            if new and vecino != lsp['from']:
-                    self.send_message(mto =vecino,mbody=object_to_json(lsp),mtype='chat')
+        for neighbour in self.neighbours:
+            if new and neighbour != lsp['from']:
+                    self.send_message(mto =neighbour,mbody=object_to_json(lsp),mtype='chat')
--- lsr/main.py
+++ lsr/main.py
@@ -1,22 +1,18 @@
-'''
-Laboratorio 3 - Algoritmos de enrutamiento
-
-Mirka Monzon 18139
-Oscar De Leon 19298
-Andres Quinto 19288
-'''
-
 from getpass import getpass
-from lsr import LSR
+from lsr import *
+from lsr import LSRClient
 
-#Main
+"""
+    Main class
+"""
+
 if __name__ == '__main__':
-    print("\n--- Chatea :) ---\n")
-    jid = input('Ingresa tu jid: ')
-    password = getpass('Ingresa tu contraseña: ')
-    name = input('Ingresa tu nombre(s): ')
-    topolo = input('Ingresa tu topologia: ')
+    print("\n--- Welcome to the Chat! ---\n")
+    jid = input('Type your jid: ')
+    password = getpass('Type your password: ')
+    name_file = input('Type your names file: ')
+    topo_file = input('Type your topology file: ')
 
-    xmpp = LSR(jid, password,topolo,name)
+    xmpp = LSRClient(jid, password,topo_file,name_file)
     xmpp.connect()
     xmpp.process(forever=False)
