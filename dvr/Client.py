d6721823fc8f6b4753d9af24759d8fe7522e305b
Oscar <owdelc@gmail.com>
2022-09-01 18:23:52 -0600

    ready dvr

--- dvr/Client.py
+++ dvr/Client.py
@@ -1,128 +1,129 @@
-
-'''
-Laboratorio 3 - Algoritmos de enrutamiento
-
-Mirka Monzon 18139
-Oscar De Leon 19298
-Andres Quinto 19288
-'''
-
+from distanceVectorRouting import DistanceVectorRouting
 import asyncio
+from datetime import datetime
 import slixmpp
 import networkx as nx
 import ast
-from datetime import datetime
-from dvRouting import DVR as DVR
 
-
-# Inicializacion de clase Cliente
+#Inicializacion de clase cliente
 class Client(slixmpp.ClientXMPP):
-    def __init__(self, jid, contrasenia, algoritmo, nodo, nodos, nombres, graph, graph_directorio, origen):
-        super().__init__(jid, contrasenia)
-        self.recibido = set()
+    def __init__(self, jid, password, algoritmo, nodo, nodes, names, graph, graph_dict, source):
+        super().__init__(jid, password)
+        self.received = set()
         self.algoritmo = algoritmo
-        self.nombres = nombres
+        self.names = names
         self.graph = graph
-        
-        self.dvr = DVR(graph, graph_directorio, origen, nombres)
-
+        self.dvr = DistanceVectorRouting(graph, graph_dict, source, names)
         self.nodo = nodo
-        self.nodos = nodos
-        self.schedule(name="echo", callback=self.echoMensaje, seconds=5, repeat=True)
-        self.schedule(name="update", callback=self.actualizarMensaje, seconds=10, repeat=True)
-
-        self.evento_conectado = asyncio.Event()
-        self.presencia_recibida = asyncio.Event()
-
-        self.add_event_handler('session_start', self.iniciar)
-        self.add_event_handler('message', self.mensaje)
-
-        self.register_plugin('xep_0045')
-        self.register_plugin('xep_0199')
-        self.register_plugin('xep_0030')
+        self.nodes = nodes
+        self.schedule(name="echo", callback=self.echo_message, seconds=5, repeat=True)
+        self.schedule(name="update", callback=self.update_message, seconds=10, repeat=True)
         
+        self.connected_event = asyncio.Event()
+        self.presences_received = asyncio.Event()
 
-    # Inicializacion 
-    async def iniciar(self, evento):
-        self.send_presence()
+        self.add_event_handler('session_start', self.start)
+        self.add_event_handler('message', self.message)
+    
+        self.register_plugin('xep_0030') # Service Discovery
+        self.register_plugin('xep_0045') # Multi-User Chat
+        self.register_plugin('xep_0199') # Ping
+
+
+    # Incializacion
+    async def start(self, event):
+        self.send_presence() 
         await self.get_roster()
-        self.evento_conectado.set()
+        self.connected_event.set()
 
+    # Definicion mensaje
+    async def message(self, msg):
+        if msg['type'] in ('normal', 'chat'):
+            await self.reply_message(msg['body'])
 
-    # Mensaje 
-    async def mensaje(self, texto):
-        if texto['type'] in ('nornmal','chat'):
-            await self.contestar_mensaje(texto['body'])
-        
-
-    # Se realiza el proceso de contestar mensaje
-    async def contestarMensaje(self, texto):
-        mensaje = texto.split('|')
-        if mensaje[0] == '1':
+    # Contestar mensaje
+    async def reply_message(self, msg):
+        message = msg.split('|')
+        if message[0] == '1':
             if self.algoritmo == '1':
-                if mensaje[2] == self.jid:
-                    print("Mensaaje entrante --> " + mensaje[6])
+                if message[2] == self.jid:
+                    print("Incoming message >> " +  message[6])
                 else:
-                    nodoVecinoMasCercano = self.dvr.caminoCorto(mensaje[2])
-                    if nodoVecinoMasCercano:
-                        if nodoVecinoMasCercano[1] in self.dvr.vecinos:
-                            strMensaje = "!".join(mensaje)
-                            self.send_message(mto=mensaje[2],mbody=strMensaje,mtype='chat')
+                    shortest_neighbor_node = self.dvr.shortest_path(message[2])
+                    if shortest_neighbor_node:
+                        if shortest_neighbor_node[1] in self.dvr.neighbors:
+                            # We send the message
+                            StrMessage = "|".join(message)
+                            self.send_message(
+                                mto=message[2],
+                                mbody=StrMessage,
+                                mtype='chat' 
+                            )
                         else:
                             pass
                     else:
                         pass
-
-        elif mensaje[0] == '2':
+        elif message[0] == '2':
             if self.algoritmo == '1':
-                esquemaRecibido = mensaje[6]
-                dividido = esquemaRecibido.split('-')
-                aristas = ast.literal_eval(dividido[1])
-                nodos = ast.literal_eval(dividido[0])
+                esquemaRecibido = message[6]
 
+                divido = esquemaRecibido.split('-')
+                nodos = ast.literal_eval(divido[0])
+                aristas = ast.literal_eval(divido[1])
                 self.graph.add_nodes_from(nodos)
                 self.graph.add_weighted_edges_from(aristas)
 
-                self.dvr.actualizacion(nx.to_dict_of_dicts(self.graph))
+                self.dvr.update_graph(nx.to_dict_of_dicts(self.graph))
 
-                datosVecinos = self.graph.nodos().data()
-                datosEdges = self.graph.edges.data('weight')
+                dataneighbors = self.graph.nodes().data()
+                dataedges = self.graph.edges.data('weight')
+                StrNodes = str(dataneighbors) + "-" + str(dataedges)
 
-                strNodos = str(datosVecinos) + "-" + str(datosEdges)
-
-                for i in self.dvr.vecinos:
-                    cambioMensaje = "2|" + str(self.jid) + "|" + str(self.nombres[i]) + "|" + str(self.graph.number_of_nodes()) + "||" + str(self.nodo) + strNodos
-                    self.send_message(mto=self.dvr.nombres['config'][i], mbody=cambioMensaje, mtype='chat')
-
-        elif mensaje[0] == '3':
-            if mensaje[6] == '':
-                ahora = datetime.now()
-                tiempo = datetime.timestamp(ahora)
-                mensaje = texto+str(tiempo)
-                self.send_message(mto=mensaje[1], mbody=mensaje, mtype='chat')
-            
+                for i in self.dvr.neighbors:
+                    update_msg = "2|" + str(self.jid) + "|" + str(self.names[i]) + "|" + str(self.graph.number_of_nodes()) + "||" + str(self.nodo) + "|" + StrNodes
+                    self.send_message(
+                            mto=self.dvr.names['config'][i],
+                            mbody=update_msg,
+                            mtype='chat'
+                        )
+        elif message[0] == '3':
+            if message[6] == '':
+                now = datetime.now()
+                timestamp = datetime.timestamp(now)
+                mensaje = msg + str(timestamp)
+                self.send_message(
+                            mto=message[1],
+                            mbody=mensaje,
+                            mtype='chat' 
+                        )
             else:
-                diferencia = float(mensaje[6]) - float(mensaje[4])
-                self.graph[self.nodo][mensaje[5]]['weight'] = diferencia
-
+                difference = float(message[6]) - float(message[4])
+                self.graph[self.nodo][message[5]]['weight'] = difference
         else:
             pass
 
-    # Echo mensaje
-    def echoMensaje(self):
-        for i in self.nodos:
-            ahora = datetime.now()
-            tiempo = datetime.timestamp(ahora)
-            mensaje = "3|" + str(self.jid) + "|" + str(self.nombres[i]) + "||"+ str(tiempo) +"|" + str(i) + "|"
-            self.send_message(mto=self.nombres[i], mbody=mensaje, mtype='chat')
+    # Mensaje echo
+    def echo_message(self):
+        for i in self.nodes:
+            now = datetime.now()
+            timestamp = datetime.timestamp(now)
+            mensaje = "3|" + str(self.jid) + "|" + str(self.names[i]) + "||"+ str(timestamp) +"|" + str(i) + "|"
+            self.send_message(
+                        mto=self.names[i],
+                        mbody=mensaje,
+                        mtype='chat' 
+                    )
 
-    # Se realiza actualizacion del mensaje
-    def actualizarMensaje(self):
-        if self.algoritmo == "1":
-            datosVecinos = self.graph.nodes().data()
-            datosEdges = self.graph.edges.data('weight')
-            strNodos = str(datosVecinos) + "-" + str(datosEdges)
-            for i in self.dvr.vecinos:
-                nuevoMensaje = "2|" + str(self.jid) + "|" + str(self.nombres[i]) + "|" + str(self.graph.number_of_nodes()) + "||" + str(self.nodo) + "|" + strNodos
-                self.send_message(mto=self.dvr.nombres[i], mbody=nuevoMensaje, mtype='chat')
-
+    # Actualizacion de mensaje
+    def update_message(self):
+        if self.algoritmo == '1':
+            dataneighbors = self.graph.nodes().data()
+            dataedges = self.graph.edges.data('weight')
+            StrNodes = str(dataneighbors) + "-" + str(dataedges)
+            for i in self.dvr.neighbors:
+                update_msg = "2|" + str(self.jid) + "|" + str(self.names[i]) + "|" + str(self.graph.number_of_nodes()) + "||" + str(self.nodo) + "|" + StrNodes
+                self.send_message(
+                        mto=self.dvr.names[i],
+                        mbody=update_msg,
+                        mtype='chat'
+                    )
--- dvr/main.py
+++ dvr/main.py
@@ -1,95 +1,83 @@
-'''
-Laboratorio 3 - Algoritmos de enrutamiento
-
-Mirka Monzon 18139
-Oscar De Leon 19298
-Andres Quinto 19288
-'''
-
-from Client import Client
-from aioconsole import ainput
-import networkx as nx
-import yaml 
-from optparse import OptionParser
 import getpass
+from optparse import OptionParser
+import yaml
+import networkx as nx
+from client import Client
+from aioconsole import ainput
 
 
-# Se carga la configuracion de topologia y nombres
-def cargarConfiguracion():
+# Carga de confifuracion 
+def loadConfig():
+    lector_topo = open("topology.txt", "r", encoding="utf8")
+    lector_names = open("names.txt", "r", encoding="utf8")
+    topo_string = lector_topo.read()
+    names_string = lector_names.read()
+    topo_yaml = yaml.load(topo_string, Loader=yaml.FullLoader)
+    names_yaml = yaml.load(names_string, Loader=yaml.FullLoader)
+    return topo_yaml, names_yaml
 
-    lectorTopologia = open("topology.txt", "r", encoding="utf8")
-    lectorNombres = open("names.txt", "r", encoding="utf8")
+# Obtencion de nodos
+def getNodes(topo, names, user):
+    for key, value in names["config"].items():
+        if user == value:
+            return key, topo["config"][key]
 
-    stringTopologia = lectorTopologia.read()
-    stringNombres = lectorNombres.read()
-
-    yamlTopologia = yaml.load(stringTopologia, Loader=yaml.FullLoader)
-    yamlNombres = yaml.load(stringNombres, Loader=yaml.FullLoader)
-    return yamlTopologia, yamlNombres
-
-
-# Se obtienen los nodos 
-def obtenerNodos(topologia, nombres, usuario):
-    for key, value in nombres["config"].items():
-        if usuario == value:
-            return key, topologia["config"][key]
-
-
-# Se realiza el grafico 
-def obtenerGrafico(topologia, nombres, usuario):
-    graph = {}
-    origen = None
-
-    for key, value in topologia['config'].items():
-        graph[key] = {}
-        for nodo in value:
-            graph[key][nodo] = float('inf')
-            if nombres['config'][nodo] == usuario:
-                origen = nodo
+# Obtencion de grafos
+def getGraph(topo, names, user):
+    '''Build graph in python a dict'''
     
-    return graph, origen
+    graph = {}
+    source = None
 
-# Se realiza la prueba del grafico
-def pruebaGrafo(topologia, nombres):
+    for key, value in topo['config'].items():
+        graph[key] = {}
+        for node in value:
+            graph[key][node] = float('inf')
+            if names['config'][node] == user:
+                source = node
+    
+    return graph, source
+
+
+# Prueba de grafos
+def pruebaGrafo(topo, names):
     G = nx.DiGraph()
     G.add_nodes_from(G.nodes(data=True))
     G.add_edges_from(G.edges(data=True))
-    for key, value in nombres["config"].items():
+    for key, value in names["config"].items():
         G.add_node(key, jid=value)
         
-    for key, value in topologia["config"].items():
+    for key, value in topo["config"].items():
         for i in value:
             G.add_edge(key, i, weight=1)
     
     return G
-
-
+    
 # Main
 async def main(xmpp: Client):
-    run = True
-    while run:
+    corriendo = True
+    while corriendo:
         print("""
-        Bienvenido!
+        Welcome to the chat!
 
-        0. Mensaje
-        1. Salir
-        
+        0. Message
+        1. Exit
         """)
-        opcion = await ainput("Ingrese una opcion: ")
+        opcion = await ainput("Type an option: ")
         if opcion == '0':
-            destinatario = await ainput("Ingrese el usuario receptor:  ")
+            destinatario = await ainput("Type the recipient's JID:  ")
             activo = True
             while activo:
-                mensaje = await ainput("Ingrese su mensaje o atras para regresar:  ")
-                if (mensaje != 'atras') and len(mensaje) > 0:
+                mensaje = await ainput("Type your message (type back to get back):  ")
+                if (mensaje != 'back') and len(mensaje) > 0:
                     if xmpp.algoritmo == '1':
                         mensaje = "1|" + str(xmpp.jid) + "|" + str(destinatario) + "|" + str(xmpp.graph.number_of_nodes()) + "||" + str(xmpp.nodo) + "|" + str(mensaje)
-                        shortest_neighbor_node = xmpp.dvr.caminoCorto(destinatario)
+                        shortest_neighbor_node = xmpp.dvr.shortest_path(destinatario)
                         if shortest_neighbor_node:
-                            if shortest_neighbor_node[1] in xmpp.dvr.obtenerVecinos:
+                            if shortest_neighbor_node[1] in xmpp.dvr.neighbors:
 
                                 xmpp.send_message(
-                                    mto=xmpp.nombres[shortest_neighbor_node[1]],
+                                    mto=xmpp.names[shortest_neighbor_node[1]],
                                     mbody=mensaje,
                                     mtype='chat' 
                                 )
@@ -103,12 +91,12 @@ async def main(xmpp: Client):
                             mbody=mensaje,
                             mtype='chat' 
                         )
-                elif mensaje == 'atras':
+                elif mensaje == 'back':
                     activo = False
                 else:
                     pass
         elif opcion == '1':
-            run = False
+            corriendo = False
             xmpp.disconnect()
         else:
             pass
@@ -116,38 +104,40 @@ async def main(xmpp: Client):
 
 if __name__ == "__main__":
 
+
+    # Parser
     optp = OptionParser()
     optp.add_option("-j", "--jid", dest="jid",
-                    help="JID a utilizar")
+                    help="JID to use")
     optp.add_option("-p", "--password", dest="password",
-                    help="contrasenia a utilizar")
+                    help="password to use")
     optp.add_option("-a", "--algoritmo", dest="algoritmo",
-                    help="algoritmo a utilizar")
+                    help="algoritmo to use")
     
     opts, args = optp.parse_args()
 
-    topologia, nombres = cargarConfiguracion()
+    topo, names = loadConfig()
     if opts.jid is None:
-        opts.jid = input("Escriba su JID: ")
+        opts.jid = input("Type your JID: ")
     if opts.password is None:
-        opts.password = getpass.getpass("Escriba su contrasenia: ")
+        opts.password = getpass.getpass("Type your passowrd: ")
     if opts.algoritmo is None:
-        print("""Algoritmos disponibles:
+        print("""Algorithms Implemented:
         
         1. Distance Vector Routing
         
         """)
-        opts.algoritmo = input("Escriba el numero de algoritmo a utilizar: ")
+        opts.algoritmo = input("Type the number of the algorithm you want to use: ")
 
-    graph_dict, source = obtenerGrafico(topologia, nombres, opts.jid)
+    graph_dict, source = getGraph(topo, names, user=opts.jid)
 
-    nodo, nodos = obtenerNodos(topologia, nombres, opts.jid)
+    nodo, nodes = getNodes(topo, names, opts.jid)
 
-    graph = pruebaGrafo(topologia, nombres)
+    graph = pruebaGrafo(topo, names)
 
-    xmpp = Client(opts.jid, opts.password, opts.algoritmo, nodo, nodos, nombres["config"], graph, graph_dict, source)
+    xmpp = Client(opts.jid, opts.password, opts.algoritmo, nodo, nodes, names["config"], graph, graph_dict, source)
     xmpp.connect() 
     xmpp.loop.run_until_complete(xmpp.connected_event.wait())
     xmpp.loop.create_task(main(xmpp))
     xmpp.process(forever=False)
-
+    
